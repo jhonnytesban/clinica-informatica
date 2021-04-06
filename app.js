8986357c3ad751a50de6fe22bb11941bb647cd15
Jonathan <jonathanestebangalan@gmail.com>
2021-04-06 09:28:50 +0200

    fin del proyecto web

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const menu = document.getElementById('menu')
 const nav = document.getElementById('nav')
 const reparaciones = document.getElementById('linkReparaciones')
 const catalogo = document.getElementById('linkCatalogo')
+const info = document.getElementById('linkInfo')
 menu.addEventListener('click', () => {
   nav.classList.toggle('nav-activated')
   nav.classList.toggle('nav-disabled')
@@ -11,6 +12,10 @@ reparaciones.addEventListener('click', () => {
   nav.classList.toggle('nav-disabled')
 })
 catalogo.addEventListener('click', () => {
+  nav.classList.toggle('nav-activated')
+  nav.classList.toggle('nav-disabled')
+})
+info.addEventListener('click', () => {
   nav.classList.toggle('nav-activated')
   nav.classList.toggle('nav-disabled')
 })
