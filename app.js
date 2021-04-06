fa97a5555ba56a874b93c3cb9148182d24de0935
Jonathan <jonathanestebangalan@gmail.com>
2021-04-06 09:22:13 +0200

    retoques en el scrool

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const menu = document.getElementById('menu')
 const nav = document.getElementById('nav')
+const reparaciones = document.getElementById('linkReparaciones')
+const catalogo = document.getElementById('linkCatalogo')
 menu.addEventListener('click', () => {
+  nav.classList.toggle('nav-activated')
+  nav.classList.toggle('nav-disabled')
+})
+reparaciones.addEventListener('click', () => {
+  nav.classList.toggle('nav-activated')
+  nav.classList.toggle('nav-disabled')
+})
+catalogo.addEventListener('click', () => {
   nav.classList.toggle('nav-activated')
   nav.classList.toggle('nav-disabled')
 })
